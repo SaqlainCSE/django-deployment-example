97b8c8b935adc7fad9ec4616af906bfa745d18d6
SaqlainCSE <56887504+SaqlainCSE@users.noreply.github.com>
2020-05-02 00:51:58 +0600

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gnttq8p*t2h&*+safzoremwd2k%63opiaue3gpf^a+@$)-3hq6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saqlainmustak.pythonanywhere.com']
 
 
 # Application definition
